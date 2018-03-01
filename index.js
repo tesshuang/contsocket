3d40de3d8507891077644f3326dfb93e4b717528
Tess <tesshuangxj@yahoo.com>
2018-02-28 16:41:17 -0800

    edit data room

--- index.js
+++ index.js
@@ -5,33 +5,47 @@ var io = require("socket.io")(server);
 var allusers ={};
 
 io.on("connection", function(socket){
-    console.log("connected");
+        console.log("connected");
     
     socket.on("joinroom", function(data){
         console.log(data);
-        socket.join(data);
+        socket.join(data.roomstr);
         
-        socket.myRoom = data;
+        socket.myRoom = data.roomstr;
         /*socket.emit("yourid", socket.id);*/
         
-        if(!allusers[data]){
-            allusers[data] = [];
+        if(!allusers[data.roomstr]){
+            allusers[data.roomstr] = [];
             
         }
         
-        allusers[data].push(socket.id);
-        console.log(allusers);
-        
-        if(allusers[data].length === 1){
-            io.to(data).emit("waiting");
-        }else if(allusers[data].length === 2){
-             io.to(data).emit("startgame");
-        }else if(allusers[data].length > 2){
-          io.to(socket.id).emit("toomany");
+        var conusrinfo ={
+            id: socket.id,
+            conname:data.usrinfo.conname,
+            conava:data.usrinfo.conava
         }
         
-        io.to(data).emit("userjoined", allusers[data]);
-    })
+        allusers[data.roomstr].push(conusrinfo);
+        console.log(allusers);
+        
+        if(allusers[data.roomstr].length === 1){
+            io.to(data.roomstr).emit("waiting");
+        }else if(allusers[data.roomstr].length === 2){
+             io.to(data.roomstr).emit("startgame");
+        }else if(allusers[data.roomstr].length > 2){
+             socket.emit("toomany");
+        }
+        
+        io.to(data.roomstr).emit("userjoined", allusers[data.roomstr]);
+    });
+    
+    socket.on("disconnect", function(data){
+       if(this.myRoom){
+            var index = allusers[this.myRoom].indexOf(socket.id);
+            allusers[this.myRoom].splice(index,1);
+            io.to(this.myRoom).emit("userjoined", allusers[this.myRoom]);
+        }
+    });
 });
 
 server.listen(port, (err)=>{
