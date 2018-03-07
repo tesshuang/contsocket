15de3dd72b26923e294ab0d64fbb6ce8ca84eb3b
Tess <tesshuangxj@yahoo.com>
2018-03-06 21:42:17 -0800

    change emit data

--- index.js
+++ index.js
@@ -29,21 +29,21 @@ io.on("connection", function(socket){
         console.log(allusers);
         
         if(allusers[data.roomstr].length === 1){
-            io.to(data.roomstr).emit("waiting");
+            io.to(data.roomstr).emit("waiting", allusers[data.roomstr]);
         }else if(allusers[data.roomstr].length === 2){
-             io.to(data.roomstr).emit("startgame");
+             io.to(data.roomstr).emit("startgame", allusers[data.roomstr]);
         }else if(allusers[data.roomstr].length > 2){
              socket.emit("toomany");
         }
         
-        io.to(data.roomstr).emit("userjoined", allusers[data.roomstr]);
+        /*io.to(data.roomstr).emit("userjoined", allusers[data.roomstr]);*/
     });
     
     socket.on("disconnect", function(data){
        if(this.myRoom){
             var index = allusers[this.myRoom].indexOf(socket.id);
             allusers[this.myRoom].splice(index,1);
-            io.to(this.myRoom).emit("userjoined", allusers[this.myRoom]);
+            /*io.to(this.myRoom).emit("userjoined", allusers[this.myRoom]);*/
         }
     });
 });
